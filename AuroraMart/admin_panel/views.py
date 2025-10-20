ddd12e4cb426954633ac1db1d9fe0b8326ea61f9
Lian-Mao <67960963+Lian-Mao@users.noreply.github.com>
2025-10-20 16:18:52 +0800

    added admin login and main page

--- AuroraMart/admin_panel/views.py
+++ AuroraMart/admin_panel/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Welcome to the Admin Panel")
+def login(request):
+    return render(request, 'admin_panel/login.html')
+
+def signup(request):
+    return render(request, 'admin_panel/signup.html')
+
+def index(request):
+    return render(request, 'admin_panel/index.html')
+
+
