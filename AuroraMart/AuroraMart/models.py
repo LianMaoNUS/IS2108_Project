f162a625a65cdeb212e6469a73ebdeef724995f6
Lian-Mao <67960963+Lian-Mao@users.noreply.github.com>
2025-10-31 10:58:46 +0800

    light changes

--- AuroraMart/AuroraMart/models.py
+++ AuroraMart/AuroraMart/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.hashers import make_password, check_password
 
+#image
 class User(models.Model): 
     username = models.CharField(max_length=150, unique=True)
     password = models.CharField(max_length=128)
