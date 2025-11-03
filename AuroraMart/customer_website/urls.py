27648b33ea059308615094cc409c554cd5ed0625
daniel <danielang619@hotmail.com>
2025-11-03 13:13:27 +0800

    Add customer website: main page, signup, login, products, cart, checkout, profile, about pages

--- AuroraMart/customer_website/urls.py
+++ AuroraMart/customer_website/urls.py
@@ -1,7 +1,16 @@
-# admin_panel/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='customer_home'),
+    path('', views.main_page, name='main_page'),
+    path('signup/', views.signup_page, name='signup'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('home/', views.customer_home, name='customer_home'),
+    path('cart/', views.cart_page, name='cart'),
+    path('checkout/', views.checkout_page, name='checkout'),
+    path('profile/', views.profile_page, name='profile'),
+    path('product/<str:sku>/', views.product_detail, name='product_detail'),
+    path('about/', views.about_page, name='about'),
+    path('products/', views.all_products, name='all_products'),
 ]
